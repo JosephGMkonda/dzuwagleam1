d48a41a9476e097f90a8e6100696f0baefffbad6
sangwaniKondowe <josephgmkonda@gmail.com>
2024-01-04 15:39:37 +0400

    updates

--- index.js
+++ index.js
@@ -131,3 +131,22 @@ function toggleMenu() {
 
 
 ////Banner function//////
+const counts = document.querySelectorAll('.count')
+const speed = 200
+
+counts.forEach((counter)=>{
+   function upData(){
+    const target = Number(counter.getAttribute('data-target'))
+    const count = Number(counter.innerText)
+    const inc = target/ speed
+
+    if( count < target){
+        counter.innerText = Math.floor(inc + count)
+        setTimeout(upData, 1)
+    }else{
+        counter.innerText = target
+    }
+
+   }
+   upData()
+})
